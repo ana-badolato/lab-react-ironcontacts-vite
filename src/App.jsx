11639c38e534e0b613071f39ee9d0a6d6979226f
Ana Badolato <badolatoana@gmail.com>
2024-09-19 00:30:57 +0200

    Iteration 1 & 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,42 @@
+import React, { useState } from "react";
 import "./App.css";
+import contactsData from "./contacts.json";
 
 function App() {
+  const [contacts, setContacts] = useState(contactsData.slice(0, 5));
+
   return (
     <div className="App">
       <h1>LAB | React IronContacts</h1>
+      
+      <table>
+        <thead>
+          <tr>
+            <th>Picture</th>
+            <th>Name</th>
+            <th>Popularity</th>
+            <th>Oscar</th>
+            <th>Emmy</th>
+          </tr>
+        </thead>
+        <tbody>
+          {contacts.map((contact) => (
+            <tr key={contact.id}>
+              <td className="tdImg">
+                <img 
+                  src={contact.pictureUrl}
+                  alt={contact.name}
+                  style={{ width: "100px", height: "150px" }}
+                />
+              </td>
+              <td>{contact.name}</td>
+              <td>{contact.popularity.toFixed(2)}</td>
+              <td>{contact.wonOscar ? "🏆" : ""}</td>
+              <td>{contact.wonEmmy ? "🌟" : ""}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
